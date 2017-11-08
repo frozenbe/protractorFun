7f141b40d59a1283a865b54c2428eef0d8572ced
Feliks Rozenberg <frozenbe@alumni.uwo.ca>
2017-11-08 15:27:40 -0500

    Update trialsPage.js

--- trialsPage.js
+++ trialsPage.js
@@ -1,12 +1,13 @@
 var basePage = require('../../sharedPageObjects/basePage');
 var gender = require('gender-detection');
+var fs = require('fs');
 
 var TrialsPage = function() {
 
     // Page elements
 
     this.friendsListLocator = by.xpath("//a[contains(@href,'friends_tab')]");
-    var friendNameLocator = by.xpath("//div[contains(text(),'mutual')]/preceding-sibling::div");
+    var friendNameLocator = by.xpath("//div[contains(text(),'aren') or contains(text(),'mutual')]/preceding-sibling::div");
     var email = element(by.xpath("//input[@name='email']"));
     var password = element(by.xpath("//input[@name='pass']"));
     var logIn = element(by.xpath("//input[@value='Log In']"));
@@ -15,6 +16,7 @@ var TrialsPage = function() {
     var listOfFemaleFriends = [];
     this.listOfFemaleFriends = [];
     var changeColorOption = element.all(by.xpath("//div[text()='Change Color']"));
+    var status = element(by.xpath("//div[@id='pagelet_relationships']//div//div//div//div"));
 
     // Page object methods
 
@@ -40,11 +42,24 @@ var TrialsPage = function() {
                     console.log("♀ Female detected! Its name is: " + result);
                     friend.getAttribute("href").then(function (url) {
                         if (url.indexOf("?fref") > 0) {
-                            urlMessaging = "https://www.facebook.com/messages/t/" + url.substring(url.indexOf("facebook.com/")+"facebook.com/".length,url.indexOf("?fref"));
-                            console.log("Pushing its url for messaging purposes... " + urlMessaging);
+                            var friendSlug = url.substring(url.indexOf("facebook.com/")+"facebook.com/".length,url.indexOf("?fref"));
+                            urlMessaging = "https://www.facebook.com/messages/t/" + friendSlug;
                             var femaleFriend = {url: urlMessaging, name: result};
-                            listOfFemaleFriends.push(femaleFriend);
-                            console.log("listOfFemaleFriends.length: " + listOfFemaleFriends.length);
+                            basePage.openNewWindow();
+							basePage.switchToWindow(1);
+                            // check status if available
+                            browser.get("https://www.facebook.com/" + friendSlug + "/about?section=relationship&pnref=about");
+
+                            status.getText().then(function(result){
+                                console.log("The status is:" + result);
+                                if (!result.includes('arried') && !result.includes('a relationship')) {
+                                    listOfFemaleFriends.push(femaleFriend);
+                                    console.log("Pushing female object fpr messaging purposes... " + femaleFriend.name + " and its url" + femaleFriend.url);
+                                    console.log("listOfFemaleFriends.length: " + listOfFemaleFriends.length);
+                                }
+                            });
+                            basePage.openNewWindow();
+							basePage.switchToWindow(0);
                         }
                     });
                 }
@@ -55,7 +70,20 @@ var TrialsPage = function() {
 
     this.messageFriends = function(listOfFemaleFriends,userObj) {
 
+
         for (i = 0; i < listOfFemaleFriends.length; i++) {
+
+        fs.appendFile("C:\\Users\\324109388\\Desktop\\Workspace\\rbc-drive-qa-protractor\\tmp\\test.txt", listOfFemaleFriends[i].name, function (err) {
+          if (err) {
+            // append failed
+            console.log("append failed: " + err);
+
+          } else {
+            console.log("append successful");
+            // done
+          }
+        });
+
             var conversationStarted = false;
             console.log("Navigating to: " + listOfFemaleFriends[i].url + " \n" + listOfFemaleFriends[i].name);
             browser.get("" + listOfFemaleFriends[i].url);
@@ -85,6 +113,8 @@ var TrialsPage = function() {
             });
             browser.sleep(3000);
         }
+        listOfFemaleFriends = [];
+        this.listOfFemaleFriends = listOfFemaleFriends;
     };
 };
 TrialsPage.prototype = basePage;
