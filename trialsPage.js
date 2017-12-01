cade621239035fa88dc420181a4e36444f450f07
Feliks Rozenberg <frozenbe@alumni.uwo.ca>
2017-12-01 11:28:16 -0500

    Update trialsPage.js

--- trialsPage.js
+++ trialsPage.js
@@ -8,11 +8,14 @@ var TrialsPage = function() {
 
 //    this.friendsListLocator = by.xpath("//a[contains(@href,'friends_tab')]");
     this.friendsListLocator = by.xpath("//div[contains(@class,'fwb')]/a[contains(@href,'?fref=')]");
-    this.seeMoreLink = element(by.xpath("//a[text()='See More']"));
+//    this.seeMoreLink = element(by.xpath("//a[text()='See More'][contains(@href,'recently')]"));
+    this.seeMoreLink = element(by.id("group_rhc_see_more_link"));
+
     var friendNameLocator = by.xpath("//div[contains(text(),'aren') or contains(text(),'mutual') or contains(text(),'Member')]/preceding-sibling::div");
     var email = element(by.xpath("//input[@name='email']"));
     var password = element(by.xpath("//input[@name='pass']"));
     this.searchName = element(by.xpath("//input[@placeholder='Find a member']"));
+    var poke = element(by.xpath("//a[contains(@ajaxify,'send')]"));
 
     var logIn = element(by.xpath("//input[@value='Log In']"));
     var sendMsgBtnLocator = by.xpath("//a[contains(@ajaxify,'messaging/composer')]");
@@ -50,7 +53,7 @@ var TrialsPage = function() {
             friend.getText().then(function(result){
                 g = "" + gender.detect(result);
                 var isInTargetsListIndex = targetsArray.indexOf(result);
-//                console.log("Target " +  result + " is in list index: " + isInTargetsListIndex);
+                console.log("Target " +  result + " is in list index: " + isInTargetsListIndex);
                 if (g == 'female' && isInTargetsListIndex == -1) {
 //                    console.log("♀ Female detected! Its name is: " + result);
                     friend.getAttribute("href").then(function (url) {
@@ -58,8 +61,17 @@ var TrialsPage = function() {
                             var friendSlug = url.substring(url.indexOf("facebook.com/")+"facebook.com/".length,url.indexOf("?fref"));
                             urlMessaging = "https://www.facebook.com/messages/t/" + friendSlug;
                             var femaleFriend = {url: urlMessaging, name: result};
-                            basePage.openNewWindow();
+//                            basePage.openNewWindow();
 							basePage.switchToWindow(1);
+
+							// poke if possible
+//                            browser.get("https://www.facebook.com/" + friendSlug);
+//
+//							poke.isPresent().then(function(isPresentPoke){
+//							    if (isPresentPoke) {
+//							        basePage.focusAndClick(poke);
+//							    }
+//							});
 
 							// check current city if available
                             browser.get("https://www.facebook.com/" + friendSlug + "/about?section=living&pnref=about");
@@ -67,33 +79,42 @@ var TrialsPage = function() {
                             currentCity.isPresent().then(function(isPresentCurrentCity){
 
                                 if (isPresentCurrentCity) {
-                                    currentCity.getText().then(function(result){
-                                    if (!result.includes('Toronto')) {
-                                               basePage.switchToWindow(0);
-                                               return;
+                                    currentCity.getText().then(function(resultCity){
+                                    console.log("The city is:" + resultCity);
+                                    if (resultCity.includes('Toronto') || resultCity.includes('Mississauga') || resultCity.includes('Richmond Hill') || resultCity.includes('Thornhill') || resultCity.includes('Scarborough') ) {
+                                           // check status if available
+                                           browser.get("https://www.facebook.com/" + friendSlug + "/about?section=relationship&pnref=about");
+
+                                           status.isPresent().then(function(isPresentResult){
+                                               if (isPresentResult) {
+                                                   status.getText().then(function(result){
+                                                       console.log("The status is:" + result);
+                                                       if (result.includes('Single') || result.includes('No relationship info')) {
+                                                           listOfFemaleFriends.push(femaleFriend);
+                                                           console.log("Pushing female object fpr messaging purposes... " + femaleFriend.name + " and its url" + femaleFriend.url);
+
+                                                            fs.appendFile("C:\\Users\\324109388\\Desktop\\Workspace\\rbc-drive-qa-protractor\\tmp\\" + userObj.textFile, "\n" + femaleFriend.url + "\n", function (err) {
+                                                              if (err) {
+                                                                // append failed
+                                                                console.log("append failed: " + err);
+
+                                                              } else {
+                                                                console.log("append successful");
+                                                              }
+                                                            });
+
+                                                       }
+                                                   });
+                                               }
+                                           });
                                        }
                                     });
                                 }
 
 
                             });
-                            // check status if available
-                            browser.get("https://www.facebook.com/" + friendSlug + "/about?section=relationship&pnref=about");
+//                            basePage.switchToWindow(0);
 
-                            status.isPresent().then(function(isPresentResult){
-                                if (isPresentResult) {
-                                    status.getText().then(function(result){
-//                                        console.log("The status is:" + result);
-                                        if (result.includes('Single') || result.includes('No relationship info')) {
-                                            listOfFemaleFriends.push(femaleFriend);
-//                                            console.log("Pushing female object fpr messaging purposes... " + femaleFriend.name + " and its url" + femaleFriend.url);
-//                                            console.log("listOfFemaleFriends.length: " + listOfFemaleFriends.length);
-                                        }
-                                    });
-//                                    basePage.openNewWindow();
-        							basePage.switchToWindow(0);
-                                }
-                            });
                         }
                     });
                 }
@@ -103,26 +124,18 @@ var TrialsPage = function() {
     };
 
 
-    this.messageFriends = function(listOfFemaleFriends,userObj) {
+    this.messageFriends = function(listOfFemaleFriends,userObj,countSentMessages) {
 
-        listOfFemaleFriends.sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
+//        listOfFemaleFriends.sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
+        var initialIndex = Math.floor(Math.random() * (listOfFemaleFriends.length-1));
 
-        for (i = 0; i < listOfFemaleFriends.length; i++) {
+        console.log("initialIndex: " + initialIndex);
+        for (i =  listOfFemaleFriends.length-initialIndex; i >=0; --i) {
 
-        fs.appendFile("C:\\Users\\324109388\\Desktop\\Workspace\\rbc-drive-qa-protractor\\tmp\\test.txt", listOfFemaleFriends[i].name + "\n", function (err) {
-          if (err) {
-            // append failed
-//            console.log("append failed: " + err);
 
-          } else {
-//            console.log("append successful");
-            // done
-          }
-        });
 
             var conversationStarted = false;
-//            console.log("Navigating to: " + listOfFemaleFriends[i].url + " \n" + listOfFemaleFriends[i].name);
-            browser.get("" + listOfFemaleFriends[i].url);
+            browser.get("" + listOfFemaleFriends[i]);
             browser.sleep(2000);
             basePage.focusAndClick(msgInputField);
 
@@ -130,6 +143,10 @@ var TrialsPage = function() {
 //                console.log("changeColorOption.length: " + result);
                 conversationStarted = result > 0 ? true : false;
 //                console.log("Conversation has been initiated with female? " + conversationStarted);
+                if (countSentMessages >= 19) {
+                    return;
+                }
+
                 if (conversationStarted == false) {
                     element(friendNameLocator).isPresent().then(function(present){
                     if (present) {
@@ -148,12 +165,23 @@ var TrialsPage = function() {
                             msgInputField.sendKeys(userObj.message);
                             browser.actions().sendKeys(protractor.Key.ENTER).perform();
                             browser.sleep(3000);
+                            countSentMessages++;
+                            fs.appendFile("C:\\Users\\324109388\\Desktop\\Workspace\\rbc-drive-qa-protractor\\tmp\\test.txt", "\n" + name + "\n", function (err) {
+                              if (err) {
+                                // append failed
+                                console.log("append failed: " + err);
+
+                              } else {
+                                console.log("append successful");
+                              }
+                            });
                         });
                     }
                     });
 
 
                 }
+
             });
             browser.sleep(3000);
         }
