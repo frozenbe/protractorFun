ad87788c4ded2e9b726462312b978a3fcedeb4d2
Feliks Rozenberg <frozenbe@alumni.uwo.ca>
2017-12-01 12:21:04 -0500

    Create trial.js

---
+++ trial.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = {
+       TC905: {
+            '1': {
+               clientinfo: [{
+                    textFile: '',
+                    email: '',
+                    password: '',
+                    friendUrl: '',
+                    message: '',
+                    giphy: ''
+
+               }
+               ]
+            },
+            '2': {
+               clientinfo: [
+               {
+                    textFile: '',
+                    email: '',
+                    password: '',
+                    friendUrl: '',
+                    message: '',
+                    giphy: ''
+
+               }
+               ]
+            },
+            '3': {
+               clientinfo: [
+               {
+                    textFile: '',
+                    email: '',
+                    password: '',
+                    friendUrl: '',
+                    message: '',
+                    giphy: ''
+
+               }
+               ]
+            }
+
+        },
+
+
+       TC906: {
+            '1': {
+               clientinfo: [{
+                    textFile: '',
+                    email: '',
+                    password: '',
+                    friendUrl: '',
+                    message: '',
+                    giphy: ''
+               }]
+            }
+
+
+        }
+
+}
