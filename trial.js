5b29ff9b0cc563efb367386684b26a2e82418c52
Feliks Rozenberg <frozenbe@alumni.uwo.ca>
2017-12-14 13:21:43 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,62 +1,61 @@
 'use strict';
 
 module.exports = {
-       TC905: {
-            '1': {
-               clientinfo: [{
-                    textFile: '',
-                    email: '',
-                    password: '',
-                    friendUrl: '',
-                    message: '',
-                    giphy: ''
+    TC905: {
+        '1': {
+            clientinfo: [{
+                membersCount: '',
+                textFile: '',
+                email: '',
+                password: '',
+                friendUrl: '',
+                message: '',
+                giphy: ''
 
-               }
-               ]
-            },
-            '2': {
-               clientinfo: [
-               {
-                    textFile: '',
-                    email: '',
-                    password: '',
-                    friendUrl: '',
-                    message: '',
-                    giphy: ''
-
-               }
-               ]
-            },
-            '3': {
-               clientinfo: [
-               {
-                    textFile: '',
-                    email: '',
-                    password: '',
-                    friendUrl: '',
-                    message: '',
-                    giphy: ''
-
-               }
-               ]
-            }
-
+            }]
         },
+        '2': {
+            clientinfo: [{
+                membersCount: '',
+                textFile: '',
+                email: '',
+                password: '',
+                friendUrl: '',
+                message: '',
+                giphy: ''
 
+            }]
+        },
+        '3': {
+            clientinfo: [{
+                membersCount: '',
+                textFile: '',
+                email: '',
+                password: '',
+                friendUrl: '',
+                message: '',
+                giphy: ''
 
-       TC906: {
-            '1': {
-               clientinfo: [{
-                    textFile: '',
-                    email: '',
-                    password: '',
-                    friendUrl: '',
-                    message: '',
-                    giphy: ''
-               }]
-            }
-
-
+            }]
         }
+
+    },
+
+
+    TC906: {
+        '1': {
+            clientinfo: [{
+                membersCount: '',
+                textFile: '',
+                email: '',
+                password: '',
+                friendUrl: '',
+                message: '',
+                giphy: ''
+            }]
+        }
+
+
+    }
 
 }
