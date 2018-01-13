94da98d8f3bb1348e964ea7ade1840c60c1f5253
Feliks Rozenberg <frozenbe@alumni.uwo.ca>
2018-01-12 21:33:54 -0500

    Create kijijidata.js

---
+++ kijijidata.js
@@ -0,0 +1,248 @@
+'use strict';
+
+
+module.exports = {
+       TC905: {
+
+            '1': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\n- Key Largo (1948)' +
+                                  '\n- Planes, Trains & Automobiles (1987)' +
+                                  '\n- Father\'s Little Dividend (1951)' +
+                                  '\n- The Barkleys of Broadway (1949)' +
+                                  '\n- Grumpy Old Men (1993)' +
+                                  '\n- A Christmas Special with Luciano Pavarotti (1980)' +
+                                  '\n- The Danny Thomas Show collection' +
+                                  '\n- It\'s a Wonderful Life (1946)' +
+                                  '\n- Ma and Pa Kettle on Vacation (1953)' +
+                                  '\nAll in working condition.' +
+                                  '\n$5 for entire collection.' +
+                                  '\nPickup only, at 299 Roehampton Avenue, Toronto, Ontario',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'vhs.JPG',
+                    picture2: '',
+                    price: '10',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '2': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'toaster.JPG',
+                    picture2: '',
+                    price: '5',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '3': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'revlon.JPG',
+                    picture2: '',
+                    price: '10',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '5': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'coffee.JPG',
+                    picture2: '',
+                    price: '5',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '6': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'bag.JPG',
+                    picture2: '',
+                    price: '15',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+             '8': {
+                clientinfo: [
+                {
+                     title:'',
+                     category:'',
+                     description:'' +
+                                   '\nPick up only.' +
+                                   '\n299 Roehampton Avenue.',
+                     postalCode: 'm4p 0b2',
+                     address: '299 roehampton avenue',
+                     picture1: 'dumbbell.JPG',
+                     picture2: '',
+                     price: '5',
+                     email: '',
+                     password: '',
+
+                }
+                ]
+             },
+              '9': {
+                 clientinfo: [
+                 {
+                      title:'',
+                      category:'',
+                      description:'' +
+                                    '\nPick up only.' +
+                                    '\n299 Roehampton Avenue.',
+                      postalCode: 'm4p 0b2',
+                      address: '299 roehampton avenue',
+                      picture1: 'iron.JPG',
+                      picture2: '',
+                      price: '20',
+                      email: '',
+                      password: '',
+
+                 }
+                 ]
+              },
+            '10': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'lamp.JPG',
+                    picture2: '',
+                    price: '5',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '12': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'cosmetics.JPG',
+                    picture2: '',
+                    price: '15',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '13': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nPick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'hacksaw.JPG',
+                    picture2: '',
+                    price: '10',
+                    email: '',
+                    password: '',
+
+               }
+               ]
+            },
+            '14': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\n$5 per season, or $10 for entire collection. Pick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'xfiles.JPG',
+                    picture2: '',
+                    price: '10',
+                    email: '',
+                    password: '',
+               }
+               ]
+            },
+            '15': {
+               clientinfo: [
+               {
+                    title:'',
+                    category:'',
+                    description:'' +
+                                  '\nStill have the labels and stickers. Pick up only.' +
+                                  '\n299 Roehampton Avenue.',
+                    postalCode: 'm4p 0b2',
+                    address: '299 roehampton avenue',
+                    picture1: 'skirt.JPG',
+                    picture2: '',
+                    price: '50',
+                    email: '',
+                    password: '',
+               }
+               ]
+            }
+
+
+
+        }
+
+}
